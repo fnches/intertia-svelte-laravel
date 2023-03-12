cb5fc505e757b8be50d43c5611370cc49a98cdd7
fnches <qoctopi@gmail.com>
2023-03-12 22:28:26 +1030

    initiate inertia in app-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+//from inertia/svelte/laravel guide
+import { createInertiaApp } from '@inertiajs/svelte'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+    return pages[`./Pages/${name}.svelte`]
+  },
+  setup({ el, App, props }) {
+    new App({ target: el, props })
+  },
+})
